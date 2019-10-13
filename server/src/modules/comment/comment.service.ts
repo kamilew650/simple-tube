a780d1e1cfa27d334a2540c00730101c562ddf5b
kamil.lewandowski <kamil.lewandowski@gorrion.pl>
2019-10-13 21:12:09 +0200

    aa

--- server/src/modules/comment/comment.service.ts
+++ server/src/modules/comment/comment.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import CommentInput from './dto/Comment.input'
+import Comment from '../../db/entities/Comment'
+import { InjectModel } from 'nestjs-typegoose'
+import { ModelType } from 'typegoose'
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+	constructor(@InjectModel(Comment) private readonly commentModels: ModelType<Comment>) {}
+
+	async create(commentInput: CommentInput) {
+		const comment = CommentInput.toEntity(commentInput)
+
+		const newComment = new this.commentModels(comment)
+
+		return newComment.save()
+	}
+
+	async update(commentInput: CommentInput) {
+		return null
+	}
+
+	async delete() {
+		return null
+	}
+}
